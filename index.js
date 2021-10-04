493598c22707a6c81abd7a474c76f25a3aac2293
aldoortiz128 <aldoortiz128@gmail.com>
2021-10-04 13:50:38 +0000

    Finished problems one through four

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
 //1
-function countToTen(){
-  
+function countFromOne(num){
+    for (i = 1; i < num; i++) {console.log(i);}
 }
 
 //2
-function countFromOne(){
+function countEveryOdd(num){
+    for (i = 0; i < num; i++) {if (i%2) {console.log(i);}}
   
 }
 
 //3
-function countEveryEven(){
+function isNegative(num){
+    if (num < 0) {console.log("True")}
+    else {console.log("False")};
   
 }
 
 //4
-function countEveryOdd(){
-  
+function betweenFiveandTwenty(num){
+  if (num >= 5 && num <=20) {console.log("True")}
+  else {console.log("False")};
 }
 
 //5 
-function countEvens(){
+function isAllowerCase(){
   
 }
 
+countFromOne(2);
+countEveryOdd(7);
+isNegative(3);
+betweenFiveandTwenty(2);
